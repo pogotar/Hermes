239910025413d5cbf52b6f829b9cf53efb7c6bcf
pogotar <94491918+pogotar@users.noreply.github.com>
2022-04-14 14:57:28 +0200

    cambio cartella

---
+++ es3_registered/MCM_cubic.m
@@ -0,0 +1,25 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% this function does the cubic motion curve
+%
+% intput:
+%   x = dimensionless time (csi in the slides)
+%   par = generic parameter
+%   
+% output:
+%   pos = displacement
+%   vel = speed
+%   acc = acceleration
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+function [acc, vel, pos] = MCM_cubic(x,par)
+
+app = par; % to call the main in the function in the same way
+
+% formulas on the slides
+acc = 6*(1-2*x);
+vel = 6*x*(1-x);
+pos = x*(3*x-2*x^2);
+
+
+end
+
---
+++ es3_registered/main_MCM_01.m
@@ -0,0 +1,9 @@
+clear all;  close all;  clc;
+% ------------------------------------------
+
+
+
+
+for x = 0:0.001:1
+    [acc, vel, pos]= MCM_cubic(x,par);  % result of the cubic given
+end
