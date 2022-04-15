332e9310cc89ae0cb0032b69a59ffabba03fee16
pogotar <94491918+pogotar@users.noreply.github.com>
2022-04-15 15:49:54 +0200

    fatto

--- es3_registered/MC_01/main_MC_01.m
+++ es3_registered/MC_01/main_MC_01.m
@@ -27,7 +27,7 @@ for x = 0:0.1:360
 end
 
 figure(2)
-                plot(t,p,'Color', [0.9290 0.6940 0.1250],'LineWidth',2);  grid;  title('motion curve 02   cycloidal');
+                plot(t,p,'Color', [0.9290 0.6940 0.1250],'LineWidth',2);  grid;  title('motion curve 01   cycloidal');
 hold on;        plot(t,v,'Color', [0 0.4470 0.7410], 'LineWidth',2);  grid;
 hold on;        plot(t,a,'r', 'LineWidth',2);  grid;
 
